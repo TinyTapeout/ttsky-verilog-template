06dbf64fb23bb780dc98d1e72e9e4029c07f74e9
Abhiram Sai Yegalapati <ayegalap.ucsc.edu>
2026-03-03 17:54:40 -0800

    Initial ALU Setup

---
+++ src/tt_um_alu-project.v
@@ -0,0 +1,9 @@
+module tt_um_alu4 (
+    input  wire [7:0] ui_in,
+    output reg  [7:0] uo_out,
+    input  wire [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
+    input  wire       clk,
+    input  wire       rst_n
+);
